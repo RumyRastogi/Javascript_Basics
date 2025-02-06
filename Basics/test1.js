71e9d9bd39f57f36ed62eb72a1180b1ef0515f1a
WebDeveloper <rumyrastogi@gmail.com>
2025-02-06 17:57:49 +0000

    con

--- Basics/test1.js
+++ Basics/test1.js
@@ -1,1 +1,17 @@
+/*Simple Print */
+
 console.log("Rumy's World");
+
+/* Variable */
+
+var a=6;
+var b=3;
+
+var c= a+b;
+console.log("Addition of two variable is:"+ c);
+console.log("Substraction of two variable is:"+ (a-b));
+console.log("Multiply of two variable is:"+ a*b);
+console.log("Devide of two variable is:"+ a/b);
+console.log("Remainder of two variable is:"+ a%b);
+
+
